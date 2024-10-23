60f5ca1e770bc0ddc187be8266570dc4716d961b
roshan <amalazimroshan@gmail.com>
2024-10-23 12:46:40 +0530

    rendering with MVP

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ const int depth = 225;
 
 Vec3f camera(0, 0, 3);
 Vec3f light_dir(0, 0, -1);
-Vec3f eye(1, 3, 3);
+Vec3f eye(0, 0, 3);
 Vec3f center(0, 0, 0);
 
 Matrix viewport(int x, int y, int w, int h) {
