d44b9af8ede8f27209389180239800505fe0a152
roshan <amalazimroshan@gmail.com>
2026-03-30 10:13:36 +0530

    initial commit. base

--- main.cpp
+++ main.cpp
@@ -1,87 +1,26 @@
-#include <iostream>
-#include <vector>
-
-#include "display.h"
-#include "geometry.h"
-#include "model.h"
-#include "our_gl.h"
+#include <cmath>
 #include "tgaimage.h"
 
-Model *model = NULL;
-const int width = 800;
-const int height = 800;
+constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor green   = {  0, 255,   0, 255};
+constexpr TGAColor red     = {  0,   0, 255, 255};
+constexpr TGAColor blue    = {255, 128,  64, 255};
+constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
-Vec3f light_dir(1, 1, 1);
-Vec3f eye(1, -1, 3);
-Vec3f center(0, 0, 0);
-Vec3f up(0, 1, 0);
+int main(int argc, char** argv) {
+    constexpr int width  = 64;
+    constexpr int height = 64;
+    TGAImage framebuffer(width, height, TGAImage::RGB);
 
-struct GouraudShader : public IShader {
-  Vec3f varying_intensity;  // written by vertex shader, read by fragment shader
+    int ax =  7, ay =  3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
 
-  virtual Vec4f vertex(int iface, int nthvert) {
-    Vec4f gl_Vertex = embed<4>(
-        model->vert(iface, nthvert));  // read the vertex from .obj file
-    gl_Vertex = Viewport * Projection * ModelView *
-                gl_Vertex;  // transform it to screen coordinates
-    varying_intensity[nthvert] =
-        std::max(0.f, model->normal(iface, nthvert) *
-                          light_dir);  // get diffuse lighting intensity
-    return gl_Vertex;
-  }
+    framebuffer.set(ax, ay, white);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
 
-  virtual bool fragment(Vec3f bar, TGAColor &color) {
-    float intensity =
-        varying_intensity * bar;  // interpolate intensity for the current pixel
-    color = TGAColor(255, 255, 255) * intensity;  // well duh
-    return false;  // no, we do not discard this pixel
-  }
-};
-
-int main(int argc, char **argv) {
-  if (2 == argc) {
-    model = new Model(argv[1]);
-  } else {
-    model = new Model("obj/african_head.obj");
-  }
-
-  lookat(eye, center, up);
-  viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
-  projection(-1.f / (eye - center).norm());
-  light_dir.normalize();
-
-  TGAImage image(width, height, TGAImage::RGB);
-  TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
-
-  GouraudShader shader;
-  for (int i = 0; i < model->nfaces(); i++) {
-    Vec4f screen_coords[3];
-    for (int j = 0; j < 3; j++) {
-      screen_coords[j] = shader.vertex(i, j);
-    }
-    triangle(screen_coords, shader, image, zbuffer);
-  }
-
-  image.flip_vertically();  // to place the origin in the bottom left corner of
-                            // the image
-  zbuffer.flip_vertically();
-  image.write_tga_file("output.tga");
-  zbuffer.write_tga_file("zbuffer.tga");
-
-  if (!image.read_tga_file("output.tga")) {
-    std::cerr << "Failed to read TGA file." << std::endl;
-    return -1;
-  }
-  SDL_Window *window = nullptr;
-  SDL_Renderer *renderer = nullptr;
-  if (!create_window(image.get_width(), image.get_height(), window, renderer)) {
-    return -1;
-  }
-  display_tga_image(renderer, image);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  delete model;
-  return 0;
+    framebuffer.write_tga_file("framebuffer.tga");
+    return 0;
 }
+
