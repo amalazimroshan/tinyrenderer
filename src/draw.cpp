fe8e08525fb8946a47d3e5782f8b4c432cd8ff14
roshan <amalazimroshan@gmail.com>
2026-06-06 16:02:12 +0530

    apply_viewport function for defining viewport

--- src/draw.cpp
+++ src/draw.cpp
@@ -34,3 +34,9 @@ vml::Vec2i world2screen(vml::Vec3f v, int width, int height) {
   int sy = ((-v.y) + 1.f) / 2.f * height;
   return vml::Vec2i(sx, sy);
 }
+
+vml::Vec2i apply_viewport(const Viewport& vp, vml::Vec3f v) {
+  int x = vp.xmin + (vp.xmax - vp.xmin) * (0.5f + 0.5f * v.x);
+  int y = vp.ymin + (vp.ymax - vp.ymin) * (0.5f - 0.5f * v.y);
+  return vml::Vec2i(x, y);
+}
--- src/draw.h
+++ src/draw.h
@@ -3,5 +3,10 @@
 
 #include "framebuffer.h"
 
+struct Viewport {
+  int xmin, ymin, xmax, ymax;
+};
+
+vml::Vec2i apply_viewport(const Viewport& vp, vml::Vec3f v);
 void line(vml::Vec2i p1, vml::Vec2i p2, Framebuffer& fb, Color color);
 vml::Vec2i world2screen(vml::Vec3f v, int width, int height);
