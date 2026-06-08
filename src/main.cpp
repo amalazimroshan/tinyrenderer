f5c7646a6cdca310745fa701a081f42c3c64bd75
roshan <amalazimroshan@gmail.com>
2026-06-08 11:39:41 +0530

    added fps and viewport

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <framebuffer.h>
 #include <model.h>
 
+#include <chrono>
 #include <iostream>
 
 constexpr int WINDOW_WIDTH = 960;
@@ -15,22 +16,31 @@ constexpr Color green = {0, 255, 0, 255};
 int main(int argc, char** argv) {
   init_display(WINDOW_WIDTH, WINDOW_HEIGHT);
   Framebuffer image(WINDOW_WIDTH, WINDOW_HEIGHT, black);
+  Viewport vp{100, 100, WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100};
 
   std::string model_filepath = "obj/boggie/body.obj";
   Model model(model_filepath);
   std::cerr << "Loaded " << model.nverts() << " verts, " << model.nfaces()
             << " faces" << std::endl;
 
-  for (const std::vector<int>& face : model.faces) {
-    for (size_t i = 0; i < face.size(); i++) {
-      vml::Vec3f v0 = model.verts[face[i]];
-      vml::Vec3f v1 = model.verts[face[(i + 1) % face.size()]];
-      line(world2screen(v0, WINDOW_WIDTH, WINDOW_HEIGHT),
-           world2screen(v1, WINDOW_WIDTH, WINDOW_HEIGHT), image, green);
-    }
-  }
-
+  using clock = std::chrono::high_resolution_clock;
+  auto last_frame_start = clock::now();
   while (poll_events()) {
+    auto now = clock::now();
+    float dt = std::chrono::duration_cast<std::chrono::duration<float>>(
+                   now - last_frame_start)
+                   .count();
+    last_frame_start = now;
+    std::cout << "\rfps: " << dt << "\t" << std::flush;
+
+    for (const std::vector<int>& face : model.faces) {
+      for (size_t i = 0; i < face.size(); i++) {
+        vml::Vec3f v0 = model.verts[face[i]];
+        vml::Vec3f v1 = model.verts[face[(i + 1) % face.size()]];
+        line(apply_viewport(vp, v0), apply_viewport(vp, v1), image, green);
+      }
+    }
+
     update_display(image);
   }
 
